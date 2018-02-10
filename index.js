d1eca4518bc228a3f21d582ba520979741e6c2e3
ilya.l@fiverr.com <libin.ilya@gmail.com>
2018-02-10 20:14:03 +0200

    [wip]

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ const express = require('express');
 const app = express();
 const moonmen = new Moonmen();
 
+const ORIGIN = 'https://blog.ilyalibin.com';
 
-app.get('/:width', async (req, res) => {
+app.get('/:hash_uid/:width/*', async (req, res) => {
+  debugger
+  const width = parseInt(req.params.width);
+
   const data = await moonmen.optimize(
     new Downloader({
-      url: 'https://blog.ilyalibin.com/content/images/2018/01/how-many-ghosts_coloring_page_png_468x609_q85.jpg'
+      url: `${ORIGIN}/content/images/2018/01/how-many-ghosts_coloring_page_png_468x609_q85.jpg`
     }),
     new Resizer({
-      width: parseInt(req.params.width)
+      width
     })
   );
   res.contentType(data.type);
   res.end(data.buffer);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
